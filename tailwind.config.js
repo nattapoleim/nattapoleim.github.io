6c0a3d75edf566afec3b18346ec80543c3ff6989
Nattapol Eiamsa-ard <nattapol.eim@gmail.com>
2024-07-25 07:52:40 +0700

    feat:add carousel

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        black: '#000000',
-        dark: '#484848',
-        white: '#fff',
-        bg: '#f5f5f5',
-        light: '#FAFAFA',
+        bg: '#E0E0E0',
       },
     },
     container: {
@@ -24,4 +20,32 @@ module.exports = {
     },
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#1f2937',
+          'primary-content': '#fff',
+          secondary: '#484848',
+          'secondary-content': '#d7d7d7',
+          accent: '#5b86e5',
+          'accent-content': '#030612',
+          neutral: '#fafafa',
+          'neutral-content': '#151515',
+          'base-100': '#f5f5f5',
+          'base-200': '#d5d5d5',
+          'base-300': '#b6b6b6',
+          'base-content': '#151515',
+          info: '#00dbff',
+          'info-content': '#001116',
+          success: '#4ade80',
+          'success-content': '#021206',
+          warning: '#fcd34d',
+          'warning-content': '#161002',
+          error: '#ff6978',
+          'error-content': '#160405',
+        },
+      },
+    ],
+  },
 }
