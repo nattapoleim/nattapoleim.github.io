23c0a55a3c1fc701fad3e5f2aa7819277327949e
Nattapol Eiamsa-ard <nattapol.eim@gmail.com>
2024-07-28 07:18:06 +0700

    feat:start shop page

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import { products } from './data/products/products.js'
+import { products } from '../data/products/products.js'
 
 const cardDesktop = document.getElementById('cardDesktop')
 const cardMobile = document.getElementById('cardMobile')
@@ -95,45 +95,3 @@ products.forEach((product, index) => {
     </div>
   `
 })
-
-// // init carousel after htmx get element
-// setTimeout(function () {
-//   initCarousel()
-// }, 500)
-
-// // carousel
-// function initCarousel() {
-//   const carousel = document.getElementById('carousel')
-
-//   const slides = Array.from(carousel.children)
-//   const totalSlides = slides.length
-//   let currentSlide = 0
-
-//   const nextButton = document.getElementById('nextButton')
-//   const prevButton = document.getElementById('prevButton')
-
-//   if (!nextButton || !prevButton) {
-//     console.error('Navigation elements not found')
-//     return
-//   }
-
-//   function updateCarousel() {
-//     carousel.style.transform = `translateX(-${currentSlide * 100}%)`
-//     console.log('update')
-//   }
-
-//   function nextSlide() {
-//     currentSlide = (currentSlide + 1) % totalSlides
-//     console.log('next')
-//     updateCarousel()
-//   }
-
-//   function prevSlide() {
-//     currentSlide = (currentSlide - 1 + totalSlides) % totalSlides
-//     console.log('prev')
-//     updateCarousel()
-//   }
-
-//   nextButton.addEventListener('click', nextSlide)
-//   prevButton.addEventListener('click', prevSlide)
-// }
